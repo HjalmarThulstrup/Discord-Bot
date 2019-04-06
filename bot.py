e128c9affca22b9c059eaabfc1d705ba685fb51e
unknown <hjalmar_thulstrup@hotmail.com>
2019-04-06 18:03:41 +0200

    has a 1/10 chance of calling anyone who sends a message, a dink

---
+++ bot.py
@@ -0,0 +1,17 @@
+import discord, random
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as', self.user)
+
+    async def on_message(self, message):
+        # don't respond to ourselves
+        if message.author == self.user:
+            return
+        num = random.randint(1,11)
+        if num == 7:
+            await message.channel.send("you're a dink")
+
+client = MyClient()
+
+client.run("NTY0MTA1MzM2MjI3ODg5MTUz.XKjCRg.k_Au-beD3hdAstPFBi_-raaKvss")
